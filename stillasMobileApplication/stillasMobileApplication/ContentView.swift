b53c72f0170aceb42382228b347b7a104603e26a
Aleksander Aaboen <aleksanderaaboen@outlook.com>
2022-03-08 10:20:13 +0100

    Small changes

--- stillasMobileApplication/stillasMobileApplication/ContentView.swift
+++ stillasMobileApplication/stillasMobileApplication/ContentView.swift
@@ -9,10 +9,14 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
+        Text("Hello, MBstillas!")
             .padding()
     }
 }
+
+
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
