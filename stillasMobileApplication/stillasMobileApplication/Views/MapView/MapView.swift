ff7ca56634c92be50e28d2d78e586de77fa3d002
Tormod Mork Müller <tormodmm@stud.ntnu.no>
2022-03-26 15:41:24 +0100

    Abstracted view to a separate view and removed usage of AnyView

--- stillasMobileApplication/stillasMobileApplication/Views/MapView/MapView.swift
+++ stillasMobileApplication/stillasMobileApplication/Views/MapView/MapView.swift
@@ -18,6 +18,7 @@ struct MapView: View {
     @State var searchText = ""
     @State var offset: CGFloat = 0
     @State var lastOffset: CGFloat = 0
+    
     @GestureState var gestureOffset: CGFloat = 0
     
     @State private var isInitialOffsetSet = false
@@ -27,99 +28,108 @@ struct MapView: View {
         GeometryReader { proxy in
             /// MapViewMap responsible for the map view
             MapViewMap()
-        }
-        
-        GeometryReader { proxy -> AnyView in
-            let height = proxy.frame(in: .global).height
             
-            /// Returns a new view
-            return AnyView (
-                ZStack {
-                    BlurView(style:
-                            .systemMaterial)
-                    .clipShape(CustomCorners(corners: [.topLeft, .topRight], radius: 30))
-                    
-                    VStack {
-                        Capsule()
-                            .fill(Color.white)
-                            .frame(width: 80, height: 5)
-                            .padding(.top)
-                        
-                        TextField("Search field", text: $searchText)
-                            .padding(.vertical, 10)
-                            .padding(.horizontal)
-                            .background(BlurView(style: .dark))
-                            .cornerRadius(30)
-                            .colorScheme(.dark)
-                            .padding(.top, 10)
-                    }
-                    .padding(.horizontal)
-                    .frame(maxHeight: .infinity, alignment: .top)
-                    
-                }
-                .offset(y: height - 100)
-                .offset(y: -offset > 0 ? -offset <= (height - 100) ? offset : -(height - 100) : 0)
-                .gesture(DragGesture().updating($gestureOffset, body: {value, out, _ in
-                    out = value.translation.height
-                    /// onChange() updates the offset when a gesture was performed
-                    onChange()
-                }).onEnded({ value in
-                    let maxHeight = height - 100
-                    /// When the gesture ends, update the placement of the drawer view to fixed position
-                    withAnimation {
-                        if -offset > 100 && -offset < maxHeight / 2 {
-                            offset = -(maxHeight / 3)
-                        }
-                        else if (-offset > maxHeight / 2) {
-                             offset = -maxHeight
-                        }
-                        else {
-                            offset = 0
-                        }
-                    }
-                    lastOffset = offset
-                }))
-            )
+            /// DrawerView responsible for the drawer slide
+            DrawerView()
         }
-        .ignoresSafeArea(.all, edges: .bottom)
-    }
-}
-
-/**
-    onChange() resposible for updating the offset when a gesture is performed
- */
-func onChange (){
-    DispatchQueue.main.async {
-        self.offset = gestureOffset + lastOffset
     }
 }
     
-/**
-    MapViewMap creates a view containing the map
- */
-struct MapViewMap: View {
-    /// Sets the starting location to be Gjøvik (latitude: 60.79574, longitude: 10.69155)
-    @State var region = MKCoordinateRegion (
-        center: CLLocationCoordinate2D(
-            latitude: 60.79574,
-            longitude: 10.69155
-        ),
-        /// The zoom level of the application when opened
-        /// Closer to 0 means greater zoom level
-        span: MKCoordinateSpan(
-            latitudeDelta: 0.03,
-            longitudeDelta: 0.03
+    struct DrawerView: View {
+        @State var searchText = ""
+        @State var offset: CGFloat = 0
+        @State var lastOffset: CGFloat = 0
+        @GestureState var gestureOffset: CGFloat = 0
+        @State private var isInitialOffsetSet = false
+        
+        var body: some View {
+            GeometryReader { proxy in
+                let height = proxy.frame(in: .global).height
+                    ZStack {
+                        BlurView(style:
+                                .systemMaterial)
+                        .clipShape(CustomCorners(corners: [.topLeft, .topRight], radius: 30))
+                        
+                        VStack {
+                            Capsule()
+                                .fill(Color.white)
+                                .frame(width: 80, height: 5)
+                                .padding(.top)
+                            
+                            TextField("Search field", text: $searchText)
+                                .padding(.vertical, 10)
+                                .padding(.horizontal)
+                                .background(BlurView(style: .dark))
+                                .cornerRadius(30)
+                                .colorScheme(.dark)
+                                .padding(.top, 10)
+                        }
+                        .padding(.horizontal)
+                        .frame(maxHeight: .infinity, alignment: .top)
+                        
+                    }
+                    .offset(y: height - 100)
+                    .offset(y: -offset > 0 ? -offset <= (height - 100) ? offset : -(height - 100) : 0)
+                    .gesture(DragGesture().updating($gestureOffset, body: {value, out, _ in
+                        out = value.translation.height
+                        /// onChangeDrawer() updates the offset when a gesture was performed
+                        onChangeDrawer()
+                    }).onEnded({ value in
+                        let maxHeight = height - 100
+                        /// When the gesture ends, update the placement of the drawer view to fixed position
+                        withAnimation {
+                            if -offset > 100 && -offset < maxHeight / 2 {
+                                offset = -(maxHeight / 3)
+                            }
+                            else if (-offset > maxHeight / 2) {
+                                 offset = -maxHeight
+                            }
+                            else {
+                                offset = 0
+                            }
+                        }
+                        lastOffset = offset
+                    }))
+            }
+            .ignoresSafeArea(.all, edges: .bottom)
+        }
+        
+        /**
+            onChangeDrawer() resposible for updating the offset when a gesture is performed
+         */
+        func onChangeDrawer (){
+            DispatchQueue.main.async {
+                self.offset = gestureOffset + lastOffset
+            }
+        }
+    }
+    
+    /**
+        MapViewMap creates a view containing the map
+     */
+    struct MapViewMap: View {
+        /// Sets the starting location to be Gjøvik (latitude: 60.79574, longitude: 10.69155)
+        @State var region = MKCoordinateRegion (
+            center: CLLocationCoordinate2D(
+                latitude: 60.79574,
+                longitude: 10.69155
+            ),
+            /// The zoom level of the application when opened
+            /// Closer to 0 means greater zoom level
+            span: MKCoordinateSpan(
+                latitudeDelta: 0.03,
+                longitudeDelta: 0.03
+            )
         )
-    )
-    var body: some View {
-        Map(coordinateRegion: $region)
-            .ignoresSafeArea()
+        var body: some View {
+            Map(coordinateRegion: $region)
+                .ignoresSafeArea()
+        }
     }
-}
 
-struct MapView_Previews: PreviewProvider {
-    static var previews: some View {
-        MapView()
+    struct MapView_Previews: PreviewProvider {
+        static var previews: some View {
+            MapView()
+        }
     }
-}
 }
