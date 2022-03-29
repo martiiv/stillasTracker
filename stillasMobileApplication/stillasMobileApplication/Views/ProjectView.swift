5f90355e0248349212811d6988bca939b5e18de0
Tormod Mork Müller <tormodmm@stud.ntnu.no>
2022-03-29 08:11:30 +0200

    Added skeleton of project view

---
+++ stillasMobileApplication/stillasMobileApplication/Views/ProjectView.swift
@@ -0,0 +1,20 @@
+//
+//  ProjectView.swift
+//  stillasMobileApplication
+//
+//  Created by Tormod Mork Muller on 28/03/2022.
+//
+
+import SwiftUI
+
+struct ProjectView: View {
+    var body: some View {
+        Text("Project View")
+    }
+}
+
+struct ProjectView_Previews: PreviewProvider {
+    static var previews: some View {
+        ProjectView()
+    }
+}
