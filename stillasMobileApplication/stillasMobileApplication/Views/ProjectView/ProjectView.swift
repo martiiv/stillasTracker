c96eba5df2a4fb93cdd542fecbc8e456e284a9de
Tormod Mork Müller <tormodmm@stud.ntnu.no>
2022-04-04 14:55:35 +0200

    Updated project information (project names and location) to check that annotations work.

--- stillasMobileApplication/stillasMobileApplication/Views/ProjectView/ProjectView.swift
+++ stillasMobileApplication/stillasMobileApplication/Views/ProjectView/ProjectView.swift
@@ -15,7 +15,7 @@ struct BookMark: Identifiable {
 }
 
 struct ProjectView: View {
-    let items: [BookMark] = [.example1, .example2, .example3]
+    //let items: [BookMark] = [.example1, .example2, .example3]
     
     @State var searchQuery = ""
 
@@ -54,8 +54,39 @@ struct ProjectView: View {
         
         Project(
             id: 321,
-            projectName: "Ntnu i gjøvik",
-            latitude: 60.7905060889568,
+            projectName: "CC Gjøvik",
+            latitude: 60.799530,
+            longitude: 10.693144,
+            period:
+                Period.init(startDate: "20.02.2020", endDate: "10.05.2020"),
+            size: 240,
+            state: "Active",
+            adresse:
+                Adresse.init(gate: "Piazza del Colosseo 1", postnummer: "0184", kommune: "Gjøvik kommune", fylke: "Innlandet"),
+            leier:
+                Leier(name: "NTNU", number: 639967700),
+            scaffolding:
+                Scaffolding(units: [
+                    Unit(type: "Spire",
+                        quantity:
+                            Quantity(expected: 3241, registered: 3241)),
+                    Unit(type: "Flooring",
+                         quantity:
+                            Quantity(expected: 500000, registered: 499211))]),
+            geofence:
+                Geofence(
+                    wPosition:
+                        Position(latitude: 60.799530, longitude: 10.693144),
+                    xPosition:
+                        Position(latitude: 60.799530, longitude: 10.693144),
+                    yPosition:
+                        Position(latitude: 60.799530, longitude: 10.693144),
+                    zPosition:
+                        Position(latitude: 60.799530, longitude: 10.693144))),
+        Project(
+            id: 510,
+            projectName: "Studenten Gjøvik",
+            latitude: 60.798036,
             longitude: 10.681777071532371,
             period:
                 Period.init(startDate: "20.02.2020", endDate: "10.05.2020"),
@@ -76,13 +107,44 @@ struct ProjectView: View {
             geofence:
                 Geofence(
                     wPosition:
-                        Position(latitude: 60.79077759591496, longitude: 10.683249543160402),
+                        Position(latitude: 60.798036, longitude: 10.685283),
                     xPosition:
-                        Position(latitude: 60.79077759591496, longitude: 10.683249543160402),
+                        Position(latitude: 60.798036, longitude: 10.685283),
                     yPosition:
-                        Position(latitude: 60.79077759591496, longitude: 10.683249543160402),
+                        Position(latitude: 60.798036, longitude: 10.685283),
                     zPosition:
-                        Position(latitude: 60.79077759591496, longitude: 10.683249543160402))),
+                        Position(latitude: 60.798036, longitude: 10.685283))),
+        Project(
+            id: 124,
+            projectName: "Sit Barnehage",
+            latitude: 60.787788,
+            longitude: 10.680136,
+            period:
+                Period.init(startDate: "20.02.2020", endDate: "10.05.2020"),
+            size: 240,
+            state: "Active",
+            adresse:
+                Adresse.init(gate: "Piazza del Colosseo 1", postnummer: "0184", kommune: "Gjøvik kommune", fylke: "Innlandet"),
+            leier:
+                Leier(name: "NTNU", number: 639967700),
+            scaffolding:
+                Scaffolding(units: [
+                    Unit(type: "Spire",
+                        quantity:
+                            Quantity(expected: 3241, registered: 3241)),
+                    Unit(type: "Flooring",
+                         quantity:
+                            Quantity(expected: 500000, registered: 499211))]),
+            geofence:
+                Geofence(
+                    wPosition:
+                        Position(latitude: 60.787788, longitude: 10.680136),
+                    xPosition:
+                        Position(latitude: 60.787788, longitude: 10.680136),
+                    yPosition:
+                        Position(latitude: 60.787788, longitude: 10.680136),
+                    zPosition:
+                        Position(latitude: 60.787788, longitude: 10.680136))),
     ]
 
     var body: some View {
@@ -90,7 +152,7 @@ struct ProjectView: View {
             Form {
                 Section(header: Text("All Projects")) {
                     List(searchResults) { project in
-                        NavigationLink(destination: DetailView(project: project), label: {
+                        NavigationLink(destination: ProjectDetailView(project: project), label: {
                             ProjectRow(project: project) }
                         )
                     }
@@ -98,12 +160,14 @@ struct ProjectView: View {
                     //.listStyle(.grouped)
                 }
 
+                /*
                 Section(header: Text("Second List")) {
                     List(items, children: \.items) { row in
                         Image(systemName: row.icon)
                         Text(row.name)
                     }
                 }
+                */
                 
             }
             .listStyle(.grouped)
@@ -127,7 +191,7 @@ struct ProjectRow: View {
     var body: some View {
         HStack {
             VStack(alignment: .leading) {
-                Text(project.projectName)
+                Text(project.projectName).font(.headline)
                 Text(project.period.startDate + " until " + project.period.endDate).font(.subheadline).foregroundColor(.gray)
             }
             Spacer()
@@ -136,7 +200,7 @@ struct ProjectRow: View {
         }
     }
 }
-
+/*
 struct DetailView: View {
     var project: Project
 
@@ -152,6 +216,7 @@ struct DetailView: View {
         }
     }
 }
+*/
 
 struct ProjectView_Previews: PreviewProvider {
     static var previews: some View {
@@ -159,7 +224,7 @@ struct ProjectView_Previews: PreviewProvider {
     }
 }
 
-
+/*
 extension BookMark {
     static let apple = BookMark(name: "Apple", icon: "1.circle")
     static let bbc = BookMark(name: "BBC", icon: "square.and.pencil")
@@ -170,3 +235,4 @@ extension BookMark {
     static let example2 = BookMark(name: "Recent", icon: "timer", items: [BookMark.apple, BookMark.bbc, BookMark.swift, BookMark.twitter])
     static let example3 = BookMark(name: "Recommended", icon: "hand.thumbsup", items: [BookMark.apple, BookMark.bbc, BookMark.swift, BookMark.twitter])
 }
+ */
