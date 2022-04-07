047b221f5dd82f0e2e92b4d00b7aeeb72af865ce
Martin Iversen <Martin>
2022-04-07 13:36:34 +0200

    Created homepage class homePage.go

---
+++ api/endpoints/homePage.go
@@ -0,0 +1,9 @@
+package endpoints
+
+import "net/http"
+
+func homePage(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+
+}
