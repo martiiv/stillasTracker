4a3d559c2d9202cd7fea968d0d7237836136a768
Martin Iversen <Martin>
2022-04-08 10:43:09 +0200

    FInished query specification for project and storage endpoint

--- api/endpoints/APIHandler.go
+++ api/endpoints/APIHandler.go
@@ -30,17 +30,15 @@ func Handle() {
 	router.Path(baseURL + "/unit").HandlerFunc(ScaffoldingRequest)                                               //GET POST PUT DELETE
 
 	//Project endpoint
-	router.HandleFunc(baseURL+"/project/", ProjectRequest)                     //DELETE, POST, GET
-	router.HandleFunc(baseURL+"/project/", ProjectRequest).Queries()           //DELETE, POST, GET
-	router.HandleFunc(baseURL+"/project/", ProjectRequest).Queries().Queries() //DELETE, POST, GET
-
-	router.HandleFunc(baseURL+"/project/{scaffolding}", ProjectRequest)                     //DELETE, POST, GET
-	router.HandleFunc(baseURL+"/project/{scaffolding}", ProjectRequest).Queries()           //DELETE, POST, GET
-	router.HandleFunc(baseURL+"/project/{scaffolding}", ProjectRequest).Queries().Queries() //DELETE, POST, GET
+	router.HandleFunc(baseURL+"/project/", ProjectRequest).Queries("id", "{id}").Queries("scaffolding", "{scaffolding}")     //DELETE, POST, GET
+	router.HandleFunc(baseURL+"/project/", ProjectRequest).Queries("name", "{name}").Queries("scaffolding", "{scaffolding}") //DELETE, POST, GET
+	router.HandleFunc(baseURL+"/project/", ProjectRequest).Queries("id", "{id}")                                             //DELETE, POST, GET
+	router.HandleFunc(baseURL+"/project/", ProjectRequest).Queries("name", "{name}")                                         //DELETE, POST, GET
+	router.HandleFunc(baseURL+"/project/{scaffolding}", ProjectRequest)                                                      //DELETE, POST, GET
+	router.HandleFunc(baseURL+"/project/", ProjectRequest)                                                                   //DELETE, POST, GET
 
 	//Storage endpoint
 	router.HandleFunc(baseURL+"/storage/", storageRequest)
-	router.HandleFunc(baseURL+"/storage/", storageRequest).Queries()
 
 	//Profile endpoint
 	router.HandleFunc(baseURL+"/user/", ProfileRequest)
