878ae3e90db6ae2480831f715297049bec8de470
Tormod Mork Müller <tormodmm@stud.ntnu.no>
2022-05-04 20:45:52 +0200

    Added user sign in, sign out and sign up. TODO: Redirect user when successfully signed in.

--- stillasMobileApplication/stillasMobileApplication/stillasMobileApplicationApp.swift
+++ stillasMobileApplication/stillasMobileApplication/stillasMobileApplicationApp.swift
@@ -6,12 +6,33 @@
 //
 
 import SwiftUI
-
+import Firebase
+/*
 @main
 struct stillasMobileApplicationApp: App {
     var body: some Scene {
         WindowGroup {
             ContentView()
+        }
+    }
+}*/
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        FirebaseApp.configure()
+        return true
+    }
+}
+
+@main
+struct stillasMobileApplication: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+
+    var body: some Scene {
+        WindowGroup {
+            let viewModel = AppViewModel()
+            ContentView()
+                .environmentObject(viewModel)
         }
     }
 }
